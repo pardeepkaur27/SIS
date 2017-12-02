a1a2eee41f19c612c98f919f243f86a946538bcd
jaspreet-randhawa <kjaspreet011@gmail.com>
2017-12-02 04:47:17 -0500

    Add files via upload

---
+++ Director/DirectorHome.java
@@ -0,0 +1,67 @@
+package Director;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.border.EmptyBorder;
+
+import Admin.ViewTranscript;
+import studentSystem.Main;
+
+public class DirectorHome extends JFrame {
+	viewTrans view;
+	public DirectorHome() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(250, 250, 450, 350);
+		
+		JLabel lbl1 = new JLabel(" Director Home");
+		lbl1.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lbl1.setForeground(Color.GRAY);
+		lbl1.setBounds(80, 60, 250, 30);
+		
+		JButton btnViewTranscript = new JButton("View transcript");
+		btnViewTranscript.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnViewTranscript.setBounds(100, 100, 150, 30);
+		btnViewTranscript.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			     view= new viewTrans();
+			     view.setVisible(true);
+			  		
+			}
+		});
+		
+		JButton btnViewGrades = new JButton("View Grades");
+		btnViewGrades.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnViewGrades.setBounds(100, 140, 150, 30);
+		
+		JButton btnLogout = new JButton("Logout");
+		btnLogout.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnLogout.setBounds(100,200, 150, 30);
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				new Main().setVisible(true);
+			    
+				
+			 }
+		});
+		
+		setLayout(null);
+		add(lbl1);
+		add(btnViewTranscript);
+		add(btnViewGrades);
+		add(btnLogout);
+		setVisible(true);
+		
+	}
+
+}
---
+++ Director/Logindirector.java
@@ -0,0 +1,149 @@
+package Director;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout.Alignment;
+
+import studentSystem.DB;
+import studentSystem.LoginStudent;
+import studentSystem.Student;
+import studentSystem.studentHome;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Logindirector extends JFrame{
+	
+	DirectorHome s;
+		Student student=new Student();
+	
+	public Logindirector(){
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setBounds(250, 250, 450, 350);
+	JPanel Panel = new JPanel();
+	Panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(Panel);
+	
+	JLabel lblAdminLogin = new JLabel("Director Login");
+	lblAdminLogin.setFont(new Font("Tahoma", Font.BOLD, 25));
+	lblAdminLogin.setForeground(Color.GRAY);
+	
+	JLabel lblEnterName= new JLabel("Enter Username");
+	lblEnterName.setFont(new Font("Tahoma", Font.PLAIN,15));
+	lblEnterName.setForeground(Color.GRAY);
+	
+	JLabel lblPassword= new JLabel("Enter Password");
+	lblPassword.setFont(new Font("Tahoma", Font.PLAIN,15));
+	lblPassword.setForeground(Color.GRAY);
+	
+	final JTextField textField= new JTextField();
+	
+	final JPasswordField passwordField= new JPasswordField();
+	
+	JButton btnLogin= new JButton("Login");
+	btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+	btnLogin.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e) {
+			String name= textField.getText();
+			String pssword= String.valueOf(passwordField.getPassword());
+			List<String> Unames=new ArrayList<String>();
+			List<String> Pwords=new ArrayList<String>();
+			List<String> Names=new ArrayList<String>();
+		try{
+			Connection con=DB.getConnection();
+			Statement statement = con.createStatement();
+			ResultSet rset = statement.executeQuery("SELECT id, password from DirectorDetails");
+			int i=0;
+			while(rset.next()) {
+				
+			     Unames.add(rset.getString(1));
+			     Pwords.add(rset.getString(2));
+			     i++;
+			     
+			}
+			boolean status=false;
+			for(int j=0;j<Unames.size();j++){
+			if (name.equals(Unames.get(j)) && pssword.equals(Pwords.get(j))){
+				dispose();
+				s=new DirectorHome();
+				s.setVisible(true);
+				status=true;
+				
+			}
+			    
+			}
+			if(status==false)
+			{
+				JOptionPane.showMessageDialog(Logindirector.this,"Invalid login credentials");
+				textField.setText("");
+				passwordField.setText("");}
+		}catch(SQLException ex) {
+			System.out.println("error");
+		}
+
+		
+			} 
+		
+		
+	}
+	);
+		
+	GroupLayout groupLayout= new GroupLayout(Panel);
+	Panel.setLayout(groupLayout);
+	
+	groupLayout.setAutoCreateGaps(true);
+	groupLayout.setAutoCreateContainerGaps(true);
+	
+	
+	
+	groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.CENTER)
+			.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.CENTER)
+							.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblAdminLogin))
+							.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.CENTER)
+											.addComponent(lblEnterName)
+											.addComponent(lblPassword))
+									.addGroup(groupLayout.createParallelGroup(Alignment.CENTER)
+											.addComponent(passwordField, GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+											.addComponent(textField,GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE))))
+									      //  .addContainerGap(65, Short.MAX_VALUE)
+									       // addComponent(passwordField,GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE))))
+										    .addContainerGap(70, Short.MAX_VALUE))
+			.addGroup(groupLayout.createSequentialGroup()
+			.addComponent(btnLogin, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
+			.addGap(151))
+					);
+			
+	
+	groupLayout.setVerticalGroup(
+			groupLayout.createSequentialGroup()
+			.addComponent(lblAdminLogin)
+			.addGap(40)
+			.addGroup(groupLayout.createParallelGroup(Alignment.CENTER)
+					.addComponent(lblEnterName)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+			.addGap(28)
+			.addGroup(groupLayout.createParallelGroup(Alignment.CENTER)
+					.addComponent(lblPassword)
+				    .addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+			.addGap(28)
+			 .addComponent(btnLogin)
+					
+			);
+	
+	
+	
+	}
+		
+}
